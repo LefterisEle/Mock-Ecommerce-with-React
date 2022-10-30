165229ea4b9d93be6ac32bec0a06fe0f2426fdde
LefterisEle <82530482+LefterisEle@users.noreply.github.com>
2022-10-31 00:11:04 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import {Routes, Route} from 'react-router-dom'
+import React from "react"
+import Header from "./components/Header"
+import Cart from "./pages/Cart"
+import Photos from "./pages/Photos"
+
+function App() {    
+    return (
+        <div>
+            <Header />
+            <h1>Home Page</h1>
+            <Routes  >
+                <Route path='/' element={<Photos />} />
+                <Route path='/cart' element={<Cart />} />
+            </Routes>
+        </div>
+    )
+}
+
+export default App
